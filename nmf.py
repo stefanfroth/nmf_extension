f877c0aa419de38ef7893cca7623c68cf6ef9004
Stefan Roth <stefanfroth@gmail.com>
2020-11-27 16:27:05 +0100

    migrate from pandas to numpy

--- nmf.py
+++ nmf.py
@@ -1,3 +1,4 @@
+# %%
 '''
 Write my own algorithm that performs NMF on the matrix and returns the 
 imputed matrix.
@@ -25,9 +26,9 @@ def initialize_user_feature_matrix(nr_of_users, n_components):
     np.ndarray
         The user_feature_matrix
     '''
-    return np.random.randint(1, 3, size=(nr_of_users, n_components)).astype(float)
+    return np.random.random_sample(size=(nr_of_users, n_components))*3
 
-def initialize_item_feature_matrix(nr_of_items, n_components):
+def initialize_item_feature_matrix(n_components, nr_of_items):
     '''
     Initializes the user_feature_matrix.
 
@@ -43,39 +44,59 @@ def initialize_item_feature_matrix(nr_of_items, n_components):
     np.ndarray
         The user_feature_matrix
     '''
-    return np.random.randint(1, 3, size=(n_components, nr_of_items)).astype(float)
+    return np.random.random_sample(size=(n_components, nr_of_items))*3
 
 
 def update_user_feature_matrix(user_feature_matrix, item_feature_matrix, alpha, errors):
-    # calculate the updates for user_feature_matrix
-    user_feature_matrix = pd.DataFrame(user_feature_matrix)
-    item_feature_matrix = pd.DataFrame(item_feature_matrix)
-    errors = pd.DataFrame(errors)
-    for user in user_feature_matrix.index:
-        for feature in user_feature_matrix.columns:
-            update = 0
-            old = user_feature_matrix.at[user, feature]
-            for i, error in enumerate(errors.iloc[user]):
-                if not pd.isna(error):
-                    update -= 2*alpha*error*item_feature_matrix.loc[feature].iat[i]
-            user_feature_matrix.at[user, feature] = old+update
-    return user_feature_matrix.values
+    '''
+    Calculate the updates for user_feature_matrix.
+
+    Parameters
+    ----------
+    user_feature_matrix : numpy.ndarray
+        The user_feature_matrix to update.
+    item_feature_matrix : numpy.ndarray
+        The item_feature_matrix for update calculation.
+    alpha : float
+        The learning rate.
+    errors : np.ndarray
+        The errors of the last iteration.
+    
+    Returns
+    -------
+    np.ndarray
+        The updated user_feature_matrix.
+    '''
+    updates = 2*alpha*np.dot(errors, item_feature_matrix.transpose())
+    updates[np.isnan(updates)] = 0
+    user_feature_matrix -= updates
+    return user_feature_matrix # user_feature_matrix.values
 
 
-def update_item_feature_matrix(user_feature_matrix, item_feature_matrix, alpha, errors):
-    # calculate the updates for user_feature_matrix
-    user_feature_matrix = pd.DataFrame(user_feature_matrix)
-    item_feature_matrix = pd.DataFrame(item_feature_matrix)
-    errors = pd.DataFrame(errors)
-    for feature in item_feature_matrix.index:
-        for item in item_feature_matrix.columns:
-            update = 0
-            old = item_feature_matrix.at[feature, item]
-            for i, error in enumerate(errors.iloc[:,item]):
-                if not pd.isna(error):
-                    update -= 2*alpha*error*user_feature_matrix.loc[:,feature].iat[i]
-            item_feature_matrix.at[feature, item] = old+update
-    return item_feature_matrix.values
+def update_item_feature_matrix(item_feature_matrix, user_feature_matrix, alpha, errors):
+    '''
+    Calculate the updates for user_feature_matrix.
+
+    Parameters
+    ----------
+    item_feature_matrix : numpy.ndarray
+        The item_feature_matrix to update.
+    user_feature_matrix : numpy.ndarray
+        The user_feature_matrix for update calculation.
+    alpha : float
+        The learning rate.
+    errors : np.ndarray
+        The errors of the last iteration.
+    
+    Returns
+    -------
+    np.ndarray
+        The updated user_feature_matrix.
+    '''
+    updates = 2*alpha*np.dot(user_feature_matrix.transpose(), errors)
+    updates[np.isnan(updates)] = 0
+    item_feature_matrix -= updates
+    return item_feature_matrix
 
 
 class CustomNMF(NMF):
@@ -83,12 +104,12 @@ class CustomNMF(NMF):
     def fit_transform(self, X, y=None, W=None, H=None):
         if X.isna().any().any():
             W = initialize_user_feature_matrix(X.shape[0], self.n_components)
-            H = initialize_item_feature_matrix(X.shape[1], self.n_components)
+            H = initialize_item_feature_matrix(self.n_components, X.shape[1])
             Rhat = np.dot(W, H)
             errors = Rhat - X
             for i in range(self.max_iter):
                 W = update_user_feature_matrix(W, H, self.alpha, errors)
-                H = update_item_feature_matrix(W, H, self.alpha, errors)
+                H = update_item_feature_matrix(H, W, self.alpha, errors)
                 Rhat = np.matmul(W, H)
                 errors = Rhat - X
                 print(f'We are in iteration {i}')
