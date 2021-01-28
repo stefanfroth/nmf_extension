9d2d3918c1166f560e0f9937c4b53fca88e52de6
Stefan Roth <stefanfroth@gmail.com>
2021-01-28 15:59:48 +0100

    add test for right shape of result

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -12,3 +12,16 @@ def test_running(test_data, test_model):
     '''Tests whether the CustomNMF function can be imported and run'''
     R_missing = test_data[1]
     assert isinstance(test_model.fit_transform(R_missing), np.ndarray)
+
+def test_shape(test_data, test_model):
+    '''Tests whether the resulting R_hat will have the right shape'''
+    R_missing = test_data[1]
+    P_hat = test_model.fit_transform(R_missing)
+    R_hat = np.matmul(P_hat, test_model.components_)
+    assert R_hat.shape == R_missing.shape
+
+def test_better_zero_imputation(test_data, test_model):
+    '''
+    Test whether the CustomNMF actually performs better than an imputation
+    with 0s
+    '''
